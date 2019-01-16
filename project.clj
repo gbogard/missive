cdf645fc0f87e566dad9defe175623cd3b97e9d1
Guillaume Bogard <bogardguillaume@gmail.com>
2019-01-16 16:07:29 +0100

    Added whidbey for better REPL output

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :profiles {:dev {:source-paths ["dev"]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
                              [pjstadig/humane-test-output "0.9.0"]
-                             [lein-cloverage "1.0.13"]]
+                             [lein-cloverage "1.0.13"]
+                             [mvxcvi/whidbey "2.0.0"]]
+                   :middleware [whidbey.plugin/repl-pprint]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
